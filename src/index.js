5828e864657fd4b8466a83729d873fd5fef52bea
amitch23 <andrealeemitchell@gmail.com>
2015-01-08 16:34:31 -0800

    It is alive

--- src/index.js
+++ src/index.js
@@ -1,32 +1,134 @@
-// load css
 require('./styles');
-
-// Load polyfills
-require('famous-polyfills');
-
-// import dependencies
 var Engine = require('famous/core/Engine');
+var ScrollView = require('famous/views/Scrollview');
+var Surface = require('famous/core/Surface');
 var Modifier = require('famous/core/Modifier');
 var Transform = require('famous/core/Transform');
-var ImageSurface = require('famous/surfaces/ImageSurface');
+var View = require('famous/core/View');
+var context = Engine.createContext();
 
-// create the main context
-var mainContext = Engine.createContext();
+function satColor(hue, sat, lum) {
+	  return "hsl(" + hue + ", "+sat+"%, "+lum+"%)";
+	}
 
-// your app here
-var logo = new ImageSurface({
-  size: [200, 200],
-  content: 'images/famous_logo.png',
-  classes: ['backfaceVisibility']
+function PallettePicker(options) {
+	//calls the constructor of View
+	View.call(this, options);
+
+	var surfaces = [];
+	var surface;
+
+	this.hue = options.hue;
+	var sat = 100;
+	this.lum = options.lum;
+
+	var scrollView = new ScrollView();
+	for (var i = 0; i < 300; i++) {
+	  surface = new Surface({
+	    size: [200, 100],
+	    properties: {
+	      backgroundColor: satColor(this.hue, sat, this.lum)
+	    }
+	  });
+	  surfaces.push(surface);
+	  scrollView.subscribe(surface);
+	  if (sat > 0) {
+	  	sat -= 3;
+	  }
+	  else {
+	   sat = 100;
+	  }
+	}
+
+	scrollView.sequenceFrom(surfaces);
+	var horizontal = function(offset) {
+	 return Transform.translate(offset, 0, 0);
+	// return Transform.translate(offset, offset, 0);
+	};
+	scrollView.outputFrom(horizontal);
+
+
+	var scrollModifier = new Modifier({
+	  transform: Transform.translate(0, options.height)
+	});
+
+	this.add(scrollModifier).add(scrollView);
+}
+//inherits properties from view Object
+PallettePicker.prototype = Object.create(View.prototype);
+PallettePicker.prototype.constructor = PallettePicker;
+
+//instantiate color picker views
+var orangePicker = new PallettePicker({
+	hue: 18,
+	lum: 50,
+	height: 0
+
 });
 
-var initialTime = Date.now();
-var centerSpinModifier = new Modifier({
-  align: [0.5, 0.5],
-  origin: [0.5, 0.5],
-  transform: function() {
-    return Transform.rotateY(.002 * (Date.now() - initialTime));
-  }
+var yellowPicker = new PallettePicker({
+	hue: 40,
+	lum: 50,
+	height: 100
+
 });
 
-mainContext.add(centerSpinModifier).add(logo);
+var turqPicker = new PallettePicker({
+	hue: 150,
+	lum: 50,
+	height: 200
+});
+
+var lturqPicker = new PallettePicker({
+	hue: 163,
+	lum: 50,
+	height: 300
+
+});
+
+var bluePicker = new PallettePicker({
+	hue: 200,
+	lum: 50,
+	height: 400
+
+});
+
+var purpPicker = new PallettePicker({
+	hue: 330,
+	lum: 50,
+	height: 500
+});
+
+
+var redPicker = new PallettePicker({
+	hue: 344,
+	lum: 50,
+	height: 600
+
+});
+
+var greenPicker = new PallettePicker({
+	hue: 138,
+	lum: 50,
+	height: 700
+
+});
+
+var blue2Picker = new PallettePicker({
+	hue: 221,
+	lum: 50,
+	height: 800
+});
+
+
+context.add(orangePicker);  
+context.add(yellowPicker);
+context.add(turqPicker);
+
+context.add(lturqPicker);  
+context.add(bluePicker);
+context.add(purpPicker);
+
+context.add(redPicker);  
+context.add(greenPicker);
+context.add(blue2Picker);
